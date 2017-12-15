92c612ecdb85cdbba95101b2243d1f0e4c47b1f6
Radha Patel <radhapatel99@gmail.com>
2017-12-15 03:51:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function subtract(a,b) {return a - b}
 function multiply(a,b) {return a * b}
 function divide(a,b) {return a / b}
  add (8,6);
+it('inc(n) increments n and returns the result', function() {
+  expect(inc(a)).toEqual(a + 1)
+})
+function inc(n)
