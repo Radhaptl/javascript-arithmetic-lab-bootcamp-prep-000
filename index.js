bc92b3c3ac033bc73acaa150d3ec2387c2851955
Radha Patel <radhapatel99@gmail.com>
2017-12-15 03:53:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ function divide(a,b) {return a / b}
 it('inc(n) increments n and returns the result', function() {
   expect(inc(a)).toEqual(a + 1)
 })
-function inc(n)
+('add(a, b) adds two numbers and returns the result', function() {
+  expect(add(a, b)).toEqual(a + b)
+function inc(n) {return a + 1}
