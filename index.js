c30c20de337663eecece746d91d10885860df5fc
Radha Patel <radhapatel99@gmail.com>
2017-12-15 04:22:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ function divide(a,b) {return a / b}
  add (8,6);
 function inc(n) {return n + 1}
 function dec(n) {return n - 1}
-function makeInt(n) {return parseInt(n,10)}
+function makeInt(n) {return parseFloat(n,10)}
+
