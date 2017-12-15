1fcc92dce43ead87a5d18089ce6729742e08f8d7
Radha Patel <radhapatel99@gmail.com>
2017-12-15 04:20:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,17 +5,4 @@ function divide(a,b) {return a / b}
  add (8,6);
 function inc(n) {return n + 1}
 function dec(n) {return n - 1}
-function makeInt(n) {return parseInt(n)}
-function preserveDecimal(10)
-
-describe('makeInt(n)', function() {
-  it('parses n as an integer and returns the parsed integer', function() {
-    expect(makeInt(a.toString())).toEqual(a)
-  })
-
-  it('assumes base 10', function() {
-    expect(makeInt('0x2328')).toEqual(0)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
+function makeInt(n) {return parseInt(n,10)}
